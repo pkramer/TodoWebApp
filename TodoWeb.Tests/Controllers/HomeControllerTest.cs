610614f722e3e9cc24fba5873b5aa7cce9771b8b
Phil <kramerzbox@yahoo.com>
2015-08-14 11:46:31 +0200

    fixed test string

--- TodoWeb.Tests/Controllers/HomeControllerTest.cs
+++ TodoWeb.Tests/Controllers/HomeControllerTest.cs
@@ -35,7 +35,7 @@ namespace TodoWeb.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.AreEqual("Your application description page. And some testing.", result.ViewBag.Message);
         }
 
         [Test]
